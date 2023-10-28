3f30f53cda0f2b8b38d01ca018447013a0b7aee5
Gustaf Carefall <106698658+Gustaf-C@users.noreply.github.com>
2023-10-28 16:29:18 +0800

    Update code to new undo system

--- src/retirement/main.py
+++ src/retirement/main.py
@@ -15,14 +15,13 @@ from aqt.qt import (QAction, QGroupBox, QHBoxLayout, QIcon, QLineEdit, QMenu,
 from aqt.utils import tooltip, showInfo
 
 from anki.hooks import wrap
-from anki.utils import ids2str, int_time, is_mac
+from anki.utils import is_mac
 from anki.scheduler import v3
-# from anki.collection import _Collection, LegacyReviewUndo, LegacyCheckpoint
 
 
 addon_path = dirname(__file__)
 
-verNumber = "23.10"
+VERSION_NUMBER = "23.10"
 
 
 def get_config():
@@ -39,10 +38,10 @@ def attempt_starting_refresh():
 def starting_refresh():
     refresh_config()
     if mw.RetroactiveRetiring:
-        apply_retirement_actions()
+        run_collection_retirement()
     elif mw.DailyRetiring:
         if time.time() - mw.LastMassRetirement > 86400000:
-            apply_retirement_actions()
+            run_collection_retirement()
 
 
 def refresh_config():
@@ -75,12 +74,11 @@ def add_retirement_opts(dialog: DeckOptionsDialog) -> None:
     dialog.web.eval(script.replace("HTML_CONTENT", json.dumps(html)))
 
 
-def get_progress_widget():
+def get_progress_widget(window_title: str):
     progress_widget = QWidget(None)
     progress_widget.setFixedSize(400, 70)
     progress_widget.setWindowModality(Qt.WindowModality.ApplicationModal)
-    progress_widget.setWindowIcon(QIcon(join(addon_path, 'icon.png')))
-    progress_widget.setWindowTitle("Running Mass Retirement...")
+    progress_widget.setWindowTitle(window_title)
     progress_bar = QProgressBar(progress_widget)
     if is_mac:
         progress_bar.setFixedSize(380, 50)
@@ -93,19 +91,21 @@ def get_progress_widget():
     return progress_widget, progress_bar
 
 
-def apply_retirement_actions(notes=False, show_notification=True, optimizer=False):
+def run_collection_retirement(notes=False):
     time_start = time.time()
     notes_to_delete = []
+    notes_to_tag = []
+    cards_to_suspend = []
     cards_to_move = []
-    suspended = 0
-    tagged = 0
-    total = 0
-    progress_widget, progress_bar = get_progress_widget()
+    progress_widget, progress_bar = get_progress_widget('Analyzing collection...')
+
     if not notes:
         notes = grab_col()
     progress_bar.setMinimum(0)
     progress_bar.setMaximum(len(notes))
     count = 0
+    total_num_cards_to_retire = 0
+
     for nid in notes:
         count += 1
         if count % 10 == 0:
@@ -113,69 +113,87 @@ def apply_retirement_actions(notes=False, show_notification=True, optimizer=Fals
             mw.app.processEvents()
         note = mw.col.get_note(nid)
         cards = note.cards()
-
         for card in cards:
             if card.ivl == 0:
                 continue
-            notes_to_delete, cards_to_move, suspended, tagged, total = handle_retirement_actions(
-                    card, note, notes_to_delete, cards_to_move, suspended, tagged, total)
+            notes_to_delete, notes_to_tag, cards_to_suspend, cards_to_move, retire_bool = \
+                check_retirement_actions(card, note, notes_to_delete, notes_to_tag, cards_to_suspend, cards_to_move)
+
+            if retire_bool:
+                total_num_cards_to_retire += 1
+
     notification = ''
-    ndl = len(notes_to_delete)
-    cml = len(cards_to_move)
+    num_notes_to_delete = len(notes_to_delete)
+    num_notes_to_tag = len(notes_to_tag)
+    num_cards_to_suspend = len(cards_to_suspend)
+    num_cards_to_move = len(cards_to_move)
+
     progress_widget.hide()
-    if suspended > 0:
-        notification += '- ' + str(suspended) + ' card(s) have been suspended<br>'
-    if tagged > 0:
-        notification += '- ' + str(tagged) + ' note(s) have been tagged<br>'
-    if cml > 0:
-        notification += '- ' + str(cml) + ' card(s) have been moved<br>'
-        move_to_deck(cards_to_move)
-    if ndl > 0:
-        notification += '- ' + str(ndl) + ' note(s) have been deleted<br>'
-        mw.col.remove_notes(notes_to_delete)
+
+    if total_num_cards_to_retire > 0:
+        if num_cards_to_suspend > 0:
+            notification += '- ' + str(num_cards_to_suspend) + ' card(s) have been suspended<br>'
+        if num_notes_to_tag > 0:
+            notification += '- ' + str(num_notes_to_tag) + ' note(s) have been tagged<br>'
+        if num_cards_to_move > 0:
+            notification += '- ' + str(num_cards_to_move) + ' card(s) have been moved<br>'
+        if num_notes_to_delete > 0:
+            notification += '- ' + str(num_notes_to_delete) + ' note(s) have been deleted<br>'
+
+        do_retirement_actions(notes_to_delete, notes_to_tag, cards_to_suspend, cards_to_move)
+
     time_end = time.time()
     if notification != '' and RetroNotifications:
-        display_notification('<b>' + str(total) + ' card(s) have been retired in ' + str(
+        display_notification('<b>' + str(total_num_cards_to_retire) + ' card(s) have been retired in ' + str(
                 round(time_end - time_start, 3)) + ' seconds:</b><br>' + notification)
+    elif total_num_cards_to_retire == 0 and RetroNotifications:
+        display_notification('No cards were retired.')
     mw.reset()
-    save_ass_retirement_timestamp(time.time())
+    save_retirement_timestamp(time.time())
 
 
-def handle_retirement_actions(
+def check_retirement_actions(
                 card,
                 note,
                 notes_to_delete,
-                cards_to_move,
-                suspended,
-                tagged,
-                total,
-                review=False):
+                notes_to_tag,
+                cards_to_suspend,
+                cards_to_move):
     deck_config = mw.col.decks.config_dict_for_deck_id(card.odid or card.did)
+    retire_bool = False
+
     if 'retirementOptions' in deck_config:
         config = deck_config['retirementOptions']
         if config['retire']:
             retire_interval = config['retireInterval']
             if card.ivl > retire_interval:
-                total += 1
-                if config['delete']:
-                    if note.id not in notes_to_delete:
-                        notes_to_delete.append(note.id)
+                retire_bool = True
+                if config['delete'] and note.id not in notes_to_delete:
+                    notes_to_delete.append(note)
                 else:
-                    if config['suspend']:
-                        if card.queue != -1:
-                            suspended += 1
-                            card.queue = -1
-                            mw.col.update_card(card)
+                    if config['suspend'] and card.queue != -1:
+                        cards_to_suspend.append(card)
+                    if config['tag'] and not note.has_tag(RetirementTag):
+                        notes_to_tag.append(card)
+                    if config['move'] and card.did != mw.col.decks.id(RetirementDeckName):
+                        cards_to_move.append(card)
 
-                    if config['tag']:
-                        if not note.has_tag(RetirementTag):
-                            tagged += 1
-                            note.add_tag(RetirementTag)
-                            mw.col.update_note(note)
-                    if config['move']:
-                        if card.did != mw.col.decks.id(RetirementDeckName):
-                            cards_to_move.append(card.id)
-    return notes_to_delete, cards_to_move, suspended, tagged, total
+    return notes_to_delete, notes_to_tag, cards_to_suspend, cards_to_move, retire_bool
+
+
+def do_retirement_actions(notes_to_delete, notes_to_tag, cards_to_suspend, cards_to_move) -> None:
+    if notes_to_delete:
+        notes_to_delete_ids = [note.id for note in notes_to_delete]
+        mw.col.remove_notes(notes_to_delete_ids)
+    if cards_to_suspend:
+        cards_to_suspend_ids = [card.id for card in cards_to_suspend]
+        mw.col.sched.suspend_cards(cards_to_suspend_ids)
+    if notes_to_tag:
+        notes_to_tag_ids = [note.id for note in notes_to_tag]
+        mw.col.tags.bulk_add(notes_to_tag_ids, RetirementTag)
+    if cards_to_move:
+        cards_to_move_ids = [card.id for card in cards_to_move]
+        mw.col.set_deck(cards_to_move_ids, mw.col.decks.id(RetirementDeckName))
 
 
 def display_notification(text):
@@ -186,40 +204,18 @@ def grab_col():
     return mw.col.find_notes("")
 
 
-def move_to_deck(cids, ogDeckId=False):
-    if ogDeckId:
-        did = ogDeckId
-    else:
-        did = mw.col.decks.id(RetirementDeckName)
-    if not cids:
-        return
-    deck = mw.col.decks.get(did)
-    if deck['dyn']:
-        return
-    mod = int_time()
-    usn = mw.col.usn()
-    scids = ids2str(cids)
-    mw.col.sched.remFromDyn(cids)
-    mw.col.db.execute("""update cards set usn=?, mod=?, did=? where id in """ + scids, usn, mod, did)
-
-
-def check_interval(self, card, ease):
+def check_interval(card):
     notes_to_delete = []
+    notes_to_tag = []
+    cards_to_suspend = []
     cards_to_move = []
-    suspended = 0
-    tagged = 0
-    total = 0
     note = mw.col.get_note(card.nid)
-    notes_to_delete, cards_to_move, suspended, tagged, total = handle_retirement_actions(
-            card, note, notes_to_delete, cards_to_move, suspended, tagged, total, True)
-    ndl = len(notes_to_delete)
-    cml = len(cards_to_move)
-    if suspended > 0 or tagged > 0 or cml > 0 or ndl > 0:
-        if cml > 0:
-            move_to_deck(cards_to_move)
-            mw.col.db.commit()
-        if ndl > 0:
-            mw.col.remove_notes(notes_to_delete)
+
+    notes_to_delete, notes_to_tag, cards_to_suspend, cards_to_move, retire_bool = \
+        check_retirement_actions(card, note, notes_to_delete, notes_to_tag, cards_to_suspend, cards_to_move)
+
+    if retire_bool:
+        do_retirement_actions(notes_to_delete, notes_to_tag, cards_to_suspend, cards_to_move)
         if RealNotifications:
             tooltip('The card has been retired.')
 
@@ -252,7 +248,7 @@ def save_config(wid, rdn, rt, retro_r, daily_r, real_n, retro_n):
 
 
 def testretire():
-    apply_retirement_actions()
+    run_collection_retirement()
 
 
 def open_settings():
@@ -357,7 +353,7 @@ def open_settings():
     vl.addLayout(vh6)
     load_current(rt, rdn, bg1b1, bg1b2, bg1b3, bg2b1, bg2b2, bg3b1, bg3b2)
     retirement_menu.setWindowTitle(
-            "Retirement Add-on Settings (Ver. " + verNumber + ")")
+            "Retirement Add-on Settings (Ver. " + VERSION_NUMBER + ")")
     retirement_menu.setWindowIcon(QIcon(join(addon_path, 'icon.png')))
     retirement_menu.setLayout(vl)
     retirement_menu.show()
@@ -383,7 +379,7 @@ def load_current(rt, rdn, bg1b1, bg1b2, bg1b3, bg2b1, bg2b2, bg3b1, bg3b2):
         bg3b2.setChecked(True)
 
 
-def save_ass_retirement_timestamp(timestamp):
+def save_retirement_timestamp(timestamp):
     config = get_config()
     config["Last Mass Retirement"] = timestamp
     mw.addonManager.writeConfig(__name__, config)
@@ -417,4 +413,4 @@ aqt.addons.ConfigEditor.accept = wrap(aqt.addons.ConfigEditor.accept, support_ac
 
 
 mw.refreshRetirementConfig = refresh_config
-mw.runRetirement = apply_retirement_actions
+mw.runRetirement = run_collection_retirement
